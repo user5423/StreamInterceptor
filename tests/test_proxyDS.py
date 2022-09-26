b32b2486e57d345722e9fcb2a69c4b17e0409ec2
user5423 <davide.correia@outlook.com>
2022-09-26 19:22:02 +0100

    Added tests for Buffer read()

--- tests/test_proxyDS.py
+++ tests/test_proxyDS.py
@@ -69,3 +69,110 @@ class Test_Buffer_Init:
     def test_RequestDelimiters_subsets(self) -> None:
         ## TODO: Our Buffer may reorder delimiters that are subsets of each other (need to evaluate feature)
         raise NotImplementedError()
+    
+    
+
+class Test_Buffer_ByteOperations:
+    ## read() tests
+    
+    ## NOTE: These tests are in the scenario where the number of desired bytes
+    ## to be read is LESS than the number of current bytes stored in the buffer
+    def test_read_negativeBytes(self):
+        delimiters = ["\r\n"]
+        b = Buffer(delimiters)
+        
+        b._data += b"testdata"
+        bufferLength = len(b._data)
+        
+        assert b.read(-1) == b._data
+        assert bufferLength == len(b._data)
+        assert len(b._requests) == 0
+
+        
+    def test_read_zeroBytes(self):
+        delimiters = ["\r\n"]
+        b = Buffer(delimiters)
+        
+        b._data += b"testdata"
+        bufferLength = len(b._data)
+        
+        assert b.read(0) == b._data[0:0]
+        assert bufferLength == len(b._data)
+        assert len(b._requests) == 0
+
+        
+    def test_read_oneByte(self):
+        delimiters = ["\r\n"]
+        b = Buffer(delimiters)
+        
+        b._data += b"testdata"
+        bufferLength = len(b._data)
+        
+        assert b.read(1) == b._data[0:1]
+        assert bufferLength == len(b._data)
+        assert len(b._requests) == 0
+        
+        
+    def test_read_manyBytes(self):
+        delimiters = ["\r\n"]
+        b = Buffer(delimiters)
+        
+        b._data += b"testdata"
+        bufferLength = len(b._data)
+        readLength = bufferLength // 2
+        
+        assert b.read(readLength) == b._data[0:readLength]
+        assert bufferLength == len(b._data)
+        assert len(b._requests) == 0
+        
+    
+    ## NOTE: These tests are in the scenario where the number of desired bytes
+    ## to be read EXCEEDS the number of current bytes stored in the buffer
+    def test_read_negativeBytes_zeroBuffer(self):
+        delimiters = ["\r\n"]
+        b = Buffer(delimiters)
+        
+        # b._data is initialized as empty bytearray()
+        bufferLength = len(b._data)
+        
+        assert b.read(-1) == b._data
+        assert bufferLength == len(b._data)
+        assert len(b._requests) == 0
+        
+    def test_read_zeroBytes_zeroBuffer(self):
+        delimiters = ["\r\n"]
+        b = Buffer(delimiters)
+        
+        # b._data is initialized as empty bytearray()
+        bufferLength = len(b._data)
+        
+        assert b.read(0) == bytearray()
+        assert bufferLength == 0
+        assert len(b._requests) == 0
+        
+    def test_read_oneByte_zeroBuffer(self):
+        delimiters = ["\r\n"]
+        b = Buffer(delimiters)
+        
+        # b._data is initialized as empty bytearray()
+        bufferLength = len(b._data)
+        
+        assert b.read(1) == bytearray()
+        assert bufferLength == 0
+        assert len(b._requests) == 0
+        
+        
+    def test_read_manyBytes_oneBuffer(self):
+        delimiters = ["\r\n"]
+        b = Buffer(delimiters)
+        
+        b._data += b"t"
+        bufferLength = len(b._data)
+
+        
+        assert b.read(bufferLength + 1) == b._data
+        assert bufferLength == len(b._data)
+        assert len(b._requests) == 0
+        
+    
+   
