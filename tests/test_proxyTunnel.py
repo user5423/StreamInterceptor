2020b4923253c3ea789507298b95c72f57e8b01a
user5423 <davide.correia@outlook.com>
2022-10-01 08:30:47 +0100

    Added tests for ProxyTunnel buffer selection

--- tests/test_proxyTunnel.py
+++ tests/test_proxyTunnel.py
@@ -31,9 +31,13 @@ class PTTestResources:
         return mockStreamInterceptor
 
     @staticmethod
-    def createSocket():
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        return s
+    def createSocket() -> socket.socket:
+        return socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+    @staticmethod
+    def closeSocket(s: socket) -> None:
+        s.close()
+
 
 
 class Test_ProxyTunnel_Init:
@@ -47,15 +51,40 @@ class Test_ProxyTunnel_Init:
     
 
     def test_init_duplicatedSockets(self):
-        ...
+        clientToProxySocket = PTTestResources.createSocket()
+        proxyToServerSocket = clientToProxySocket
+        streamInterceptor = PTTestResources.createMockStreamInterceptor()
+
+        with pytest.raises(ValueError) as excInfo:
+            ProxyTunnel(clientToProxySocket, proxyToServerSocket, streamInterceptor)
+
+        assert "duplicate sockets" in str(excInfo.value)
 
 
-    def test_init_deadSocket_ClientToProxy(self):
-        ...
+    def test_init_closedSocket_ClientToProxy(self):
+        clientToProxySocket = PTTestResources.createSocket()
+        proxyToServerSocket = PTTestResources.createSocket()
+        streamInterceptor = PTTestResources.createMockStreamInterceptor()
+
+        PTTestResources.closeSocket(clientToProxySocket)
+        with pytest.raises(ValueError) as excInfo:
+            ProxyTunnel(clientToProxySocket, proxyToServerSocket, streamInterceptor)
+
+        assert "closed socket" in str(excInfo.value)
+        assert "ClientToServer" in str(excInfo.value)
 
 
-    def test_init_deadSocket_ProxyToServer(self):
-        ...
+    def test_init_closedSocket_ProxyToServer(self):
+        clientToProxySocket = PTTestResources.createSocket()
+        proxyToServerSocket = PTTestResources.createSocket()
+        streamInterceptor = PTTestResources.createMockStreamInterceptor()
+
+        PTTestResources.closeSocket(proxyToServerSocket)
+        with pytest.raises(ValueError) as excInfo:
+            ProxyTunnel(clientToProxySocket, proxyToServerSocket, streamInterceptor)
+
+        assert "closed socket" in str(excInfo.value)
+        assert "ProxyToServer" in str(excInfo.value)
 
 
     def test_init_correct(self):
@@ -89,9 +118,56 @@ class Test_ProxyTunnel_Init:
 class Test_ProxyTunnel_ByteOperations:
     ## ProxyTunnel().read()
     ## ProxyTunnel().write()
+    ...
 
 
 class Test_ProxyTunnel_HelperMethods:
-    ## ProxyTunnel()._selectBufferToWrite()
-    ## ProxyTunnel()._selectBufferToRead()
+    def _setupPT(self):
+        clientToProxySocket = PTTestResources.createSocket()
+        proxyToServerSocket = PTTestResources.createSocket()
+        streamInterceptor = PTTestResources.createMockStreamInterceptor()
+        pt = ProxyTunnel(clientToProxySocket, proxyToServerSocket, streamInterceptor)
+        return pt
 
+    ## Writing to a socket requires
+    ## 1. reading from OPPOSITE buffer
+    ## 2. writing to destination socket
+    def test_selectBufferForWrite_clientToProxy(self):
+        pt = self._setupPT()
+        b = pt._selectBufferForRead(pt.clientToProxySocket)
+        assert b == pt.clientToServerBuffer
+
+
+    def test_selectBufferForWrite_proxyToClient(self):
+        pt = self._setupPT()
+        buffer = pt._selectBufferForRead(pt.proxyToServerSocket)
+        assert buffer == pt.serverToClientBuffer
+
+
+    def test_selectBufferForWrite_IncorrectSocket(self):
+        pt = self._setupPT()
+        nonparticipatingSocket = PTTestResources.createSocket()
+        with pytest.raises(Exception) as excInfo:
+            pt._selectBufferForRead(nonparticipatingSocket)
+        assert "not associated with this tunnel" in str(excInfo.value)
+
+
+    ## Reading from a socket requires
+    ## 1. reading from destination socket
+    ## 2. writing to destination buffer
+    def test_selectBufferForWrite_clientToProxy(self):
+        pt = self._setupPT()
+        b = pt._selectBufferForRead(pt.clientToProxySocket)
+        assert b == pt.serverToClientBuffer
+
+    def test_selectBufferForWrite_proxyToClient(self):
+        pt = self._setupPT()
+        b = pt._selectBufferForRead(pt.proxyToServerSocket)
+        assert b == pt.clientToServerBuffer
+
+    def test_selectBufferForWrite_IncorrectSocket(self):
+        pt = self._setupPT()
+        nonparticipatingSocket = PTTestResources.createSocket()
+        with pytest.raises(Exception) as excInfo:
+            pt._selectBufferForRead(nonparticipatingSocket)
+        assert "not associated with this tunnel" in str(excInfo.value)
