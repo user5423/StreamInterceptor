648bb95e3c94bd3a27f56ce8e732a87798c0e236
user5423 <davide.correia@outlook.com>
2022-10-01 09:55:42 +0100

    Implemented socket-related setup methods for proxyTunnel tests

--- tests/test_proxyTunnel.py
+++ tests/test_proxyTunnel.py
@@ -2,6 +2,8 @@ import collections
 import os
 import sys
 import functools
+from typing import List, Tuple
+from aiohttp import ClientTimeout
 import pytest
 import socket
 
@@ -10,12 +12,15 @@ sys.path.insert(0, "src")
 from tcp_proxyserver import ProxyTunnel
 from _proxyDS import Buffer
 
+## TODO: At some point in the future we want to 
+## perform UDP and TCP tests
 
+## TODO: Add resources for UDP and TCP connections
+## Consider testing for UDP and TCP on proxyTunnel
 class PTTestResources:
-    ...
+
     @staticmethod
     def createMockStreamInterceptor():
-        
         class mockStreamInterceptor:
             REQUEST_DELIMITERS = [b"\r\n"]
             def __init__(self):
@@ -31,12 +36,61 @@ class PTTestResources:
         return mockStreamInterceptor
 
     @staticmethod
-    def createSocket() -> socket.socket:
-        return socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    def createClientSocket() -> socket.socket:
+        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        s.setblocking(True)
+        return s
+
+    @staticmethod
+    def createServerSocket() -> socket.socket:
+        ## TODO: We need a method of setting up ephemeral server sockets
+        serverSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        ## NOTE: socket.bind(("",0)) passes response of PORT selection on localhost
+        ## for binding to the host operating system
+        serverSocket.bind(("", 0))
+        serverSocket.listen()
+        serverSocket.setblocking(False)
+        return serverSocket
+
+    @staticmethod
+    def acceptConnection(serverSocket: socket.socket) -> socket.socket:
+        clientToProxySocket, (hostname, port) = serverSocket.accept()
+        return clientToProxySocket
 
     @staticmethod
     def closeSocket(s: socket) -> None:
         s.close()
+
+
+    @staticmethod
+    def connect(client: socket.socket, server: socket.socket) -> socket.socket:
+        serverHost, serverPort = server.getsockname()
+        client.connect((serverHost, serverPort))
+        print("hello")
+        ephemeralSocket, _ = server.accept()
+        return ephemeralSocket
+
+    @staticmethod
+    def _setupPT() -> Tuple[ProxyTunnel, List[socket.socket]]:
+        ## Setup Client <--> Proxy connection
+        proxyServerSocket = PTTestResources.createServerSocket()
+        clientSocket = PTTestResources.createClientSocket()
+        ephemeralProxyServerSocket = PTTestResources.connect(clientSocket, proxyServerSocket)
+
+        ## Setup Proxy <--> Server connection
+        serverSocket = PTTestResources.createServerSocket()
+        proxyClientSocket = PTTestResources.createClientSocket()
+        ephemeralServerSocket = PTTestResources.connect(proxyClientSocket, serverSocket)
+
+        ## Rename the sockets
+        clientToProxySocket = ephemeralProxyServerSocket
+        proxyToServerSocket = ephemeralServerSocket
+
+        ## Create ProxyTunnel
+        streamInterceptor = PTTestResources.createMockStreamInterceptor()
+        pt = ProxyTunnel(clientToProxySocket, proxyToServerSocket, streamInterceptor)
+        hopList = [clientSocket, clientToProxySocket, proxyToServerSocket, serverSocket]
+        return pt, hopList
 
 
 
@@ -51,7 +105,7 @@ class Test_ProxyTunnel_Init:
     
 
     def test_init_duplicatedSockets(self):
-        clientToProxySocket = PTTestResources.createSocket()
+        clientToProxySocket = PTTestResources.createClientSocket()
         proxyToServerSocket = clientToProxySocket
         streamInterceptor = PTTestResources.createMockStreamInterceptor()
 
@@ -62,8 +116,8 @@ class Test_ProxyTunnel_Init:
 
 
     def test_init_closedSocket_ClientToProxy(self):
-        clientToProxySocket = PTTestResources.createSocket()
-        proxyToServerSocket = PTTestResources.createSocket()
+        clientToProxySocket = PTTestResources.createClientSocket()
+        proxyToServerSocket = PTTestResources.createClientSocket()
         streamInterceptor = PTTestResources.createMockStreamInterceptor()
 
         PTTestResources.closeSocket(clientToProxySocket)
@@ -75,8 +129,8 @@ class Test_ProxyTunnel_Init:
 
 
     def test_init_closedSocket_ProxyToServer(self):
-        clientToProxySocket = PTTestResources.createSocket()
-        proxyToServerSocket = PTTestResources.createSocket()
+        clientToProxySocket = PTTestResources.createClientSocket()
+        proxyToServerSocket = PTTestResources.createClientSocket()
         streamInterceptor = PTTestResources.createMockStreamInterceptor()
 
         PTTestResources.closeSocket(proxyToServerSocket)
@@ -88,8 +142,8 @@ class Test_ProxyTunnel_Init:
 
 
     def test_init_correct(self):
-        clientToProxySocket = PTTestResources.createSocket()
-        proxyToServerSocket = PTTestResources.createSocket()
+        clientToProxySocket = PTTestResources.createClientSocket()
+        proxyToServerSocket = PTTestResources.createClientSocket()
         streamInterceptor = PTTestResources.createMockStreamInterceptor()
 
         pt = ProxyTunnel(clientToProxySocket, proxyToServerSocket, streamInterceptor)
@@ -115,38 +169,28 @@ class Test_ProxyTunnel_Init:
     
 
 
-class Test_ProxyTunnel_ByteOperations:
-    ## ProxyTunnel().read()
-    ## ProxyTunnel().write()
-    ...
-
 
 class Test_ProxyTunnel_HelperMethods:
-    def _setupPT(self):
-        clientToProxySocket = PTTestResources.createSocket()
-        proxyToServerSocket = PTTestResources.createSocket()
-        streamInterceptor = PTTestResources.createMockStreamInterceptor()
-        pt = ProxyTunnel(clientToProxySocket, proxyToServerSocket, streamInterceptor)
-        return pt
+
 
     ## Writing to a socket requires
     ## 1. reading from OPPOSITE buffer
     ## 2. writing to destination socket
     def test_selectBufferForWrite_clientToProxy(self):
-        pt = self._setupPT()
+        pt, _ = PTTestResources._setupPT()
         b = pt._selectBufferForRead(pt.clientToProxySocket)
         assert b == pt.clientToServerBuffer
 
 
     def test_selectBufferForWrite_proxyToClient(self):
-        pt = self._setupPT()
+        pt, _ = PTTestResources._setupPT()
         buffer = pt._selectBufferForRead(pt.proxyToServerSocket)
         assert buffer == pt.serverToClientBuffer
 
 
     def test_selectBufferForWrite_IncorrectSocket(self):
-        pt = self._setupPT()
-        nonparticipatingSocket = PTTestResources.createSocket()
+        pt, _ = PTTestResources._setupPT()
+        nonparticipatingSocket = PTTestResources.createClientSocket()
         with pytest.raises(Exception) as excInfo:
             pt._selectBufferForRead(nonparticipatingSocket)
         assert "not associated with this tunnel" in str(excInfo.value)
@@ -156,18 +200,18 @@ class Test_ProxyTunnel_HelperMethods:
     ## 1. reading from destination socket
     ## 2. writing to destination buffer
     def test_selectBufferForWrite_clientToProxy(self):
-        pt = self._setupPT()
+        pt, _ = PTTestResources._setupPT()
         b = pt._selectBufferForRead(pt.clientToProxySocket)
         assert b == pt.serverToClientBuffer
 
     def test_selectBufferForWrite_proxyToClient(self):
-        pt = self._setupPT()
+        pt, _ = PTTestResources._setupPT()
         b = pt._selectBufferForRead(pt.proxyToServerSocket)
         assert b == pt.clientToServerBuffer
 
     def test_selectBufferForWrite_IncorrectSocket(self):
-        pt = self._setupPT()
-        nonparticipatingSocket = PTTestResources.createSocket()
+        pt, _ = PTTestResources._setupPT()
+        nonparticipatingSocket = PTTestResources.createClientSocket()
         with pytest.raises(Exception) as excInfo:
             pt._selectBufferForRead(nonparticipatingSocket)
         assert "not associated with this tunnel" in str(excInfo.value)
