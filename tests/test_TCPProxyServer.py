fc4dd5fc929bfdc5f5becc99575a7de1e5df4ba3
user5423 <davide.correia@outlook.com>
2022-11-03 12:06:41 +0000

    Added assertion test helpers for selector and proxyConnection states for TCPProxyServer, and additional await methods to mitigate race conditions

--- tests/test_TCPProxyServer.py
+++ tests/test_TCPProxyServer.py
@@ -6,9 +6,7 @@ import signal
 import socket
 import sys
 import threading
-from typing import Tuple
-from weakref import proxy
-from phonenumbers import expected_cost
+from typing import Tuple, List
 import pytest
 import psutil
 
@@ -39,7 +37,10 @@ class TPSTestResources:
                 self._exitFlag = False
                 self._mainThread = threading.Thread(target=self._run, args=(HOST, PORT,))
                 self._selector = selectors.DefaultSelector()
-
+                self._counterEvent = threading.Event()
+                self._counterLock = threading.Lock()
+                self._counterTarget = 0
+                self._counterCurrent = 0
 
             def _run(self, HOST, PORT) -> None:
                 # print(f"Starting Echo Server @ {HOST}:{PORT}")
@@ -60,12 +61,13 @@ class TPSTestResources:
                                 conn, addr = serverSock.accept()
                             except BlockingIOError:
                                 continue
+
+                            self._updateCounterEvent()
                             connThread = threading.Thread(target=self._serviceConnection, args=(conn,))
                             self._threads.append(connThread)
                             connThread.start()
                     except Exception as e:
                         print(f"EchoServer exception: {e}")
-
 
             def _serviceConnection(self, conn: socket.socket):
                 with conn:
@@ -91,9 +93,27 @@ class TPSTestResources:
                 self._mainThread.join()
                 ## close connection threads
                 for thread in self._threads:
-                    ## print("EchoServer - Closing child thread")
+                    # print("EchoServer - Closing child thread")
                     ## socket closing are handled by context managers
                     thread.join()
+
+            def _updateCounterEvent(self):
+                with self._counterLock:
+                    self._counterCurrent += 1
+                    if self._counterCurrent >= self._counterTarget:
+                        self._counterEvent.set()
+
+            def awaitConnectionCount(self, count: int):
+                with self._counterLock:
+                    self._counterTarget = count
+                    if self._counterCurrent >= self._counterTarget:
+                        self._counterEvent.set()
+                    else:
+                        self._counterEvent.clear()
+            
+                self._counterEvent.wait()
+                # print("returned from await")
+                return None
 
         return EchoServer(HOST, PORT)
 
@@ -115,7 +135,7 @@ class TPSTestResources:
         return thread
 
     @classmethod
-    def _assertConstantAttributes(cls, server: TCPProxyServer,
+    def assertConstantAttributes(cls, server: TCPProxyServer,
         HOST, PORT, PROXY_HOST, PROXY_PORT, interceptor) -> None:
         ## NOTE: There some ambiguity about which host refers to the
         ## proxy interceptor and which one refers to the target service
@@ -140,11 +160,88 @@ class TPSTestResources:
         assert server.proxyConnections.PROXY_PORT == server.PROXY_PORT
         assert server.proxyConnections.selector == server.selector
 
-
         ## We need to check signal handlers have been set
         ## NOTE: SIGNAL types are dependent on the host system
         ## --> We'll focus on linux
         assert signal.getsignal(signal.SIGINT) == server._sigHandler
+
+    @classmethod
+    def _retrieveTunnelSocksFromSelector(cls, proxyServer):
+        registeredConnections = {}
+        for _, key in proxyServer.selector.get_map().items():
+            sock = key.fileobj
+            if proxyServer.serverSocket != sock:
+                registeredConnections[(sock.getsockname(), sock.getpeername())] = sock
+        return registeredConnections
+
+
+    @classmethod
+    def assertSelectorState(cls, proxyServer: TCPProxyServer, connections: List[socket.socket]) -> None:
+        ## it should be added to the selector (correct  key, no duplicates)
+        ## There should be two fd's socket server, and new connection and proxy
+        ## We check that the selector has the correct number of fd being polled
+        assert len(proxyServer.selector.get_map()) == 1 + len(connections)*2 ## serverSock + 2 endpoints for each connection
+
+        ## checking server socket is logged
+        serverSocketKey = proxyServer.selector.get_key(proxyServer.serverSocket)
+        assert serverSocketKey.data == "ServerSocket"
+        assert serverSocketKey.events == selectors.EVENT_READ
+
+        ## We create a set based on the peernames of the descriptors (then we can look them up)
+        ## NOTE: Be careful - There may be some weirdness (e.g. "" is equal to "0.0.0.0")
+        registeredSockNames = cls._retrieveTunnelSocksFromSelector(proxyServer)
+
+        ## for each user connection, there should be two sockets managed by server 
+        ## (this ensure equal number of types of sockets (clientTOProxy, and ProxyToServer))
+        assert len(proxyServer.selector.get_map()) == 1 + len(connections)*2 ## serverSock + 2 endpoints for each connection
+        assert len(registeredSockNames) == len(connections) * 2
+
+        ## Here we perform assertions on the client <--> proxy connections
+        for conn in connections:
+            connKey = (conn.getpeername(), conn.getsockname())
+            assert connKey in registeredSockNames
+            registeredSockNames.pop(connKey) ## remove from dict
+
+        ## Here we perform assertions on the proxy <--> server connections
+        for sock in registeredSockNames.values():
+            ## The peer should be the destination server for all proxy <--> server connections
+            peername = sock.getpeername()
+            assert peername[0] == proxyServer.PROXY_HOST
+            assert peername[1] == proxyServer.PROXY_PORT
+
+
+    @classmethod
+    def assertProxyConnectionsState(cls, proxyServer: TCPProxyServer, connections: List[socket.socket]) -> None:
+        ## proxyServer.proxyConnections contains two socket keys for each tunnel (hence multiply by 2)
+        assert len(proxyServer.proxyConnections._sock) == len(connections) * 2
+
+        ## NOTE: We will NOT take a look at the internal state of the proxyTunnels
+        tunnels = {}
+        for sock, tunnel in proxyServer.proxyConnections._sock.items():
+            if tunnels.get(tunnel) is None:
+                tunnels[tunnel] = set()
+            tunnels[tunnel].add(sock)
+
+        ## validate that each tunnel has two elements and that they are linked to the tunnel, 
+        ## they are not closed, and have correct peernames / socknames
+        for tunnel, socks in tunnels.items():
+            assert len(socks) == 2
+            assert tunnel.clientToProxySocket in socks
+            assert tunnel.clientToProxySocket.fileno() != -1
+            ## NOTE: Eventhough serverSock.accept() creates an ephemeral socket (it seems the new socket retains the same sockname as serverSock)
+            assert tunnel.clientToProxySocket.getsockname() == proxyServer.serverSocket.getsockname()
+
+            assert tunnel.proxyToServerSocket in socks
+            assert tunnel.proxyToServerSocket.fileno() != -1 # if socket is closed, socket.fileno() == -1
+            peerAddress =  tunnel.proxyToServerSocket.getpeername()
+            assert peerAddress[0] == proxyServer.PROXY_HOST
+            assert peerAddress[1] == proxyServer.PROXY_PORT
+            
+
+    @classmethod
+    def assertProxyTunnelsState(cls, proxyServer: TCPProxyServer, connections: List[socket.socket], connectionData: List[str]):
+        ## NOTE: Here we can evaluate the state of the bytes stored in the proxyTunnels
+        ...
 
 
 
@@ -177,14 +274,20 @@ def createSingleThreadTCPProxyServer(createTCPProxyServer):
         def run(self):
             return self.thread.start()
 
+        def awaitConnectionCount(self, count: int) -> None:
+            ## we'll use a spinlock to avoid modifying the class for the sake of testing
+            while len(self.proxyServer.proxyConnections) != count * 2: ## for each user connection, the server manages 2 connections
+                pass
+            return None
+
+
     proxyServerWrapper = threadWrapper(proxyServer)
     yield HOST, PORT, PROXY_HOST, PROXY_PORT, interceptor, proxyServerWrapper
 
-    # print("TCPProxyServer closing")
-    proxyServer.close()
-    # print("TCPProxyServer exitFlag set")
+    print("TCPProxyServer closing")
+    proxyServer.close(blocking=True)
     proxyServerWrapper.thread.join()
-    # print("TCPProxyServer closed")
+    print("TCPProxyServer closed")
 
 
 @pytest.fixture()
@@ -193,9 +296,9 @@ def createBackendEchoServer(createTCPProxyServer):
     echoServer = TPSTestResources.setupEchoServer(PROXY_HOST, PROXY_PORT)
     yield echoServer
 
-    # print("EchoServer closing")
+    print("EchoServer closing")
     echoServer.close()
-    # print("EchoServer closed")
+    print("EchoServer closed")
 
 
 
@@ -237,7 +340,7 @@ class Test_ProxyServer_Init:
 
         ## check that the selector has been created correctly
         assert server.selector == server.proxyConnections.selector
-        TPSTestResources._assertConstantAttributes(server, HOST, PORT, PROXY_HOST, PROXY_PORT, interceptor)
+        TPSTestResources.assertConstantAttributes(server, HOST, PORT, PROXY_HOST, PROXY_PORT, interceptor)
 
 
     def test_init_alreadyRegistered(self, createTCPProxyServer) -> None:
@@ -257,18 +360,7 @@ class Test_ProxyServer_Init:
         assert selectorKey.events == selectors.EVENT_READ
 
         ## server2 should never be created
-        TPSTestResources._assertConstantAttributes(server1, HOST, PORT, PROXY_HOST, PROXY_PORT, streamInterceptor)
-
-
-
-
-
-
-    ## two connections
-
-    ## multiple connections
-    pass
-
+        TPSTestResources.assertConstantAttributes(server1, HOST, PORT, PROXY_HOST, PROXY_PORT, streamInterceptor)
 
 class Test_ProxyServer_connectionTeardown:
     pass
