1dee9b7250a4d4bd89359f58b8760e7f4bf88574
user5423 <davide.correia@outlook.com>
2022-11-09 16:22:25 +0000

    Added DataTransferSimulator.py helper method for System tests

---
+++ tests/testhelper/DataTransferSimulator.py
@@ -0,0 +1,178 @@
+import socket
+import queue
+import random
+import datetime
+import string
+
+from typing import List, Callable, Optional
+
+class DataTransferSimulator:
+    @classmethod
+    def sendMultiConnMultiMessage(cls, connections: List[socket.socket],
+                                        completeConnSender: Optional[Callable] = None,
+                                        dataSizeSelector: Optional[Callable] = None,
+                                        messageCountSelector: Optional[Callable] = None,
+                                        chunkCountSelector: Optional[Callable] = None,
+                                        datetimesSelector: Optional[Callable] = None,
+                                        delimitersSelector: Optional[Callable] = None,
+                                        isEndDelimited: bool = None
+                                        ) -> List[bytes]:
+
+
+        ## Validate and prepar a completeConnSender function
+        completeConnSender = cls._validateArguments(completeConnSender, dataSizeSelector, messageCountSelector,
+                             chunkCountSelector, datetimesSelector, delimitersSelector, isEndDelimited)
+
+        ## we create a priority queue containing chunks
+        chunkQueue = queue.PriorityQueue()
+
+        ## we have the connectionData arr where connection[i] has sent connectionData[i]
+        connectionData = []
+
+        ## we iterate over each connection, adding to the chunkQueue
+        for conn in connections:
+            ## unpack the arguments for the compeleteConnSender function call
+            dataSize, messageCount, chunkCount, delimiters, datetimes, isEndDelimited = completeConnSender()
+
+            ## we generate data
+            generatedData = cls._generateData(dataSize)
+            
+            ## we then make the data into message (by inserting delimiters)
+            dataMessages = cls._convertStreamIntoMessages(generatedData, messageCount, delimiters, isEndDelimited)
+   
+            ## we add the new generated data to the connectionData list
+            connectionData.append(dataMessages)
+
+            ## we then select what indexes the data will be chunked at
+            dataChunks = cls._convertMessagesIntoChunks(dataMessages, chunkCount)
+            for index, chunk in enumerate(dataChunks):
+                ## we finally select the datetime for each index (  datetime_i <= datetime_i+1)
+                datetime = datetimes[index]
+
+                ##  we then append it to the priority queue 
+                chunkQueue.put((datetime, chunk, conn))
+
+        ## we then iterate over the queue and send it via the corresponding connection
+        while chunkQueue.qsize() > 0:
+            _, chunk, conn = chunkQueue.get()
+            conn.sendall(chunk)
+
+        return connectionData
+
+    @classmethod
+    def _validateArguments(cls, completeConnSender: Optional[Callable] = None,
+                                dataSizeSelector: Optional[Callable] = None,
+                                messageCountSelector: Optional[Callable] = None,
+                                chunkCountSelector: Optional[Callable] = None,
+                                datetimesSelector: Optional[Callable] = None,
+                                delimitersSelector: Optional[Callable] = None,
+                                isEndDelimited: bool = None
+                                ) -> Callable:
+
+        ## We validate the arguments
+        someSubMethodsProvided = bool(dataSizeSelector or messageCountSelector or chunkCountSelector
+                                     or datetimesSelector or delimitersSelector or (isEndDelimited is not None))
+
+        allSubMethodsProvided = bool(dataSizeSelector and messageCountSelector and chunkCountSelector
+                                     and datetimesSelector and delimitersSelector and (isEndDelimited is not None))
+
+        if someSubMethodsProvided is True and allSubMethodsProvided is False:
+            return Exception("You need to provide all submethods, if you are using submethods as your choice")
+        elif bool(allSubMethodsProvided and completeConnSender) is True:
+            return Exception("You need to either provide a completeConnSender or all the submethods, not both")
+        elif bool(allSubMethodsProvided or completeConnSender) is False:
+            return Exception("You need to either provide a completeConnSender or all the submethods, not None for both")
+
+        ## we selecting either DO_ALL method or combine singular parameter methods into a DO_ALL method
+        ## The DO_ALL method is completeConnSender
+        ## TODO: Rename to more appropriate method name
+        if allSubMethodsProvided:
+            def completeConnSender():
+                nonlocal dataSizeSelector, messageCountSelector, chunkCountSelector
+                nonlocal datetimesSelector, delimitersSelector, isEndDelimited
+                dataSize = dataSizeSelector()
+                messageCount = messageCountSelector()
+                chunkCount = chunkCountSelector()
+                delimiters = delimitersSelector()
+                ## datetime needs to be a generator (that keeps on pulling values when needed)
+                datetimes = datetimesSelector(chunkCount)
+                return (dataSize, messageCount, chunkCount, delimiters, datetimes, isEndDelimited)
+        
+        return completeConnSender
+
+    @classmethod
+    def _convertStreamIntoMessages(cls, data: bytes, messageCount: int, delimiters: List[bytes], isEndDelimited: bool) -> bytes:
+        if len(data) == 0:
+            raise Exception("Data needs to have at least length 1")
+        
+        ## we make sure that the last char is reserved for "isEndLimited" bool
+        delimiters = iter(delimiters)
+        delimitableMessageLen = len(data) - 1
+        if delimitableMessageLen < messageCount - int(isEndDelimited):
+            raise Exception("Cannot perform stream into messages as data size is to small for messageCount value specified")
+
+        indexes = sorted(random.sample(range(1, len(data)), messageCount - int(isEndDelimited)))
+
+        ## we then insert the delimiters at index to create messages
+        ## NOTE: This should work where delimiter >= 0
+        ## NOTE: indexes is required to be sorted
+        newData = bytearray()
+        left = 0
+        for right in indexes:
+            newData += data[left:right] + next(delimiters)
+            left = right
+
+        newData += data[left:]
+
+        ## In case we have an end delimiter, we add it
+        if isEndDelimited:
+            newData += next(delimiters)
+
+        return newData
+
+
+    @classmethod
+    def _convertMessagesIntoChunks(cls, dataMessages: bytes, chunkCount: int) -> List[bytes]:
+        if chunkCount > len(dataMessages):
+            raise Exception("Cannot have more chunks than size of data")
+
+        chunkIndexes = sorted(random.sample(range(1, len(dataMessages)-1), chunkCount-1))
+        dataChunks = []
+        left = 0
+        for right in chunkIndexes:
+            dataChunks.append(dataMessages[left:right])
+            left = right
+
+        dataChunks.append(dataMessages[left:])
+        return dataChunks
+
+
+    @classmethod
+    def _generateData(cls, dataSize: int) -> bytes:
+        charset = string.ascii_letters + string.digits
+        return bytes("".join((random.choice(charset) for _ in range(dataSize))), "utf-8")
+
+
+    @classmethod
+    def _datetimesSelector(cls, testTimeRange, chunkCount) -> List[datetime.datetime]:
+        ## NOTE: The list of datetimes needs to be sorted from small to high
+        startTime = datetime.datetime.now()
+        resolution = 0.01
+        sendTimes = sorted(random.choices(range(0, int(testTimeRange/resolution)+1), k=chunkCount))
+        sendTimes = [startTime + datetime.timedelta(seconds=sendTime*resolution) for sendTime in sendTimes] ## we normalize for the resolution
+        return sendTimes
+
+
+    @classmethod
+    def _delimitersSelector(cls, delimiters, messageCount):
+        return random.choices(delimiters, k=messageCount)
+
+
+    @classmethod
+    def _awaitRoundaboutConnection(cls, connections: List[socket.socket]) -> None:
+        for conn in connections:
+            conn.setblocking(True) ## we block so that we can let other threads work
+            conn.recv(1024, socket.MSG_PEEK) ## peeks at data without consuming it
+            conn.setblocking(False)
+
+
