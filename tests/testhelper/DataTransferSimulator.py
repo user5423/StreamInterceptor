20597ac1dbd00e478f11e00706d992a6a9508743
user5423 <user5423@localhost.localdomain>
2023-07-23 21:02:43 +0100

    Implemented a Telnet client simulator and a basic FTP client simulator that supports user login, control commands (non-data), and basic type retrieval data commands

--- tests/testhelper/DataTransferSimulator.py
+++ tests/testhelper/DataTransferSimulator.py
@@ -1,10 +1,17 @@
+import pprint
+import os
 import socket
-import queue
+import abc
 import random
 import datetime
 import string
+import re
+import collections
+from typing import List, Callable, Optional, Tuple, Union, FrozenSet, Dict
 
-from typing import List, Callable, Optional, Tuple, Union
+## TODO: Have not added 120 vs 220 checks on connection initiation
+## TODO: Have not considered ABORT and REIN commands
+## TODO: Have not considered STOU and storage type commands
 
 class DataTransferSimulator:
     @classmethod
@@ -194,3 +201,347 @@ class DataTransferSimulator:
 
         return completeConnSender
 
+
+
+
+
+
+class TelnetClientSimulator:
+    def _createClientConnection(self, HOST: str, PORT: int) -> socket.socket:
+        s = socket.socket()
+        s.connect((HOST, PORT))
+        return s
+
+    def _simulateCommSequence(self, clientSock: socket.socket, commandSequence: Tuple[bytes], dtsTimeout: Optional[Union[float, int]] = 0.2) -> None:
+        recvBuffer = bytearray()
+        responseSequence = []
+        responseProccessedCounter = 0
+        respondedCommandsCounter = 0
+        flattenedCommandSequence = []
+
+        for commands in commandSequence:
+            # print(f"commands: {commands}")
+            ## We execute a request callback on each command before we send them
+            for command in commands:
+                self._requestCallback(command)
+
+            ## We send all of the commands jointly
+            clientSock.settimeout(None)
+            clientSock.sendall(b"".join(commands))
+            flattenedCommandSequence.extend(commands)
+
+            clientSock.settimeout(dtsTimeout)
+            while True:
+                ## make this recv mechnaism more robust for below nested for loopnt
+                try:
+                    chunk = clientSock.recv(4096)
+                except socket.timeout:
+                    break
+
+                ## Need to check if the client has received an exit connection empty string b""
+                if len(chunk) == 0:
+                    raise Exception("Unexpected clientSock exit")
+            
+                recvBuffer += chunk
+                
+                messages = recvBuffer.splitlines(True)
+                ## messages must be at least length 1 (assuming recv() is blocking)
+                if messages[-1][-2:] == b"\r\n":
+                    responseSequence.extend(messages)
+                    recvBuffer = b""
+                else:
+                    responseSequence.extend(messages[:-1])
+                    recvBuffer = messages[-1]
+
+            
+            commandIndex = respondedCommandsCounter
+            # print(f"messages: {messages}")
+            # print(f"buffer: {recvBuffer}")
+            # print(respondedCommandsCounter)
+            # print(responseProccessedCounter)
+            ## Now we process the responses alongside their commands
+            for commandIndex, command in enumerate(flattenedCommandSequence[respondedCommandsCounter:], start=respondedCommandsCounter):
+                ## We iterate over the response index (until we find the response that matches the expectations of executing the command)
+                for _, response in enumerate(responseSequence[responseProccessedCounter:], start=responseProccessedCounter):
+
+                    ## process responses
+                    ## - this is some custom callback provided by the test case
+                    callbackResult = self._responseCallback(command, response)
+                    ## increment counter
+                    responseProccessedCounter += 1
+
+                    ## if the response callback was sucecssful, we process the next command
+                    if callbackResult:
+                        break
+
+                ## if we have processed all requests, then we should not process any commands
+                ## - this is because of the sequential nature of commands and corresponding responses
+                if responseProccessedCounter == len(responseSequence) - 1:
+                    break
+
+            ## If we exited the outer for loop early, then we have unprocessed commands
+            respondedCommandsCounter = commandIndex
+
+            ## If the last callback was unsuccesful, that means the last command processed wasn't satisifed
+            ## - so next time, we want to process that command
+            ## - However, if the last callback was successful, then the command was satisified, so we do NOT want to process it next time
+            if callbackResult:
+                respondedCommandsCounter += 1
+
+        clientSock.settimeout(None)
+        return (flattenedCommandSequence, respondedCommandsCounter), (responseSequence, responseProccessedCounter)
+        return (flattenedCommandSequence, respondedCommandsCounter), (responseSequence, responseProccessedCounter)
+
+    @abc.abstractmethod
+    def _responseCallback(self, request: bytes, response: bytes) -> None:
+        return None
+
+    @abc.abstractmethod
+    def _requestCallback(self, request: bytes) -> None:
+        return None
+
+
+
+
+## NOTE: This command simulator only implements default data connection transfer
+
+class FTPClientSimulator(TelnetClientSimulator):
+    dataConnInitiationCommands: FrozenSet[bytes] = frozenset({b"PORT", b"PASV"})
+    dataConnControlCommands: FrozenSet[bytes] = frozenset({b"PORT", b"PASV", b"ABORT"})
+    dataConnReliantCommands: FrozenSet[bytes] = frozenset({b"RETR", b"STOR", b"STOU", b"APPEND", b"LIST", b"NLST"})
+    dataRecvCommands: FrozenSet[bytes] = frozenset({b"RETR", b"LIST", b"NLST"})
+    dataSendCommands: FrozenSet[bytes] = frozenset({b"STOR", b"STOU", b"APPEND"})
+    unqueriedResponses: FrozenSet[bytes] = frozenset({b""})
+
+    ## TODO; Verify with RFC that these are correct
+    acceptableCommandFollowThrough: Dict[bytes, Dict[bytes, Tuple[bytes]]] = {
+        ## Logout
+        b"REIN": {
+            b"120": (b"220",)
+        },
+        ## File action commands
+        b"STOR": {
+            ## TODO: In the rfc, 110 is specified in brackets - is this optional? figure out if it makes a difference
+            b"125": (b"110", b"226", b"250", b"425", b"426", b"451", b"551", b"552"),
+            b"150": (b"110", b"226", b"250", b"425", b"426", b"451", b"551", b"552"),
+        },
+        b"STOU": {
+            ## TODO: In the rfc, 110 is specified in brackets - is this optional? figure out if it makes a difference
+            b"125": (b"110", b"226", b"250", b"425", b"426", b"451", b"551", b"552"),
+            b"150": (b"110", b"226", b"250", b"425", b"426", b"451", b"551", b"552"),
+        },
+        b"RETR": {
+            ## TODO: In the rfc, 110 is specified in brackets - is this optional? figure out if it makes a difference
+            b"125": (b"110", b"226", b"250", b"425", b"426", b"451"),
+            b"150": (b"110", b"226", b"250", b"425", b"426", b"451"),
+        },
+        b"LIST": {
+            b"125": (b"226", b"250", b"425", b"426", b"451"),
+            b"150": (b"226", b"250", b"425", b"426", b"451"),
+        },
+        b"NLIST": {
+            b"125": (b"226", b"250", b"425", b"426", b"451"),
+            b"150": (b"226", b"250", b"425", b"426", b"451"),
+        },
+        b"APPE": {
+            ## TODO: In the rfc, 110 is specified in brackets - is this optional? figure out if it makes a difference
+            b"125": (b"110", b"226", b"250", b"425", b"426", b"451", b"551", b"552"),
+            b"150": (b"110", b"226", b"250", b"425", b"426", b"451", b"551", b"552"),
+        }
+
+    }
+
+    ## TODO; Verify with RFC that these are correct
+    acceptableCommandResponses: Dict[bytes, bytes] = {
+        ## Login
+        b"USER": {b"230", b"530", b"500", b"501", b"421", b"331", b"332"},
+        b"PASS": {b"230", b"202", b"530", b"500", b"501", b"503", b"421", b"332"},
+        b"ACCT": {b"230", b"202", b"530", b"500", b"501", b"503", b"421"},
+        b"CWD": {b"250", b"500", b"501", b"502", b"421", b"530", b"550"},
+        b"CDUP": {b"200", b"500", b"501", b"502", b"421", b"530", b"550"},
+        b"SMNT": {b"202", b"250", b"500", b"501", b"502", b"421", b"530", b"550"},
+        ## Logout
+        b"REIN": {b"120", b"220", b"421", b"500", b"502"}, ## TODO: Has follow through response
+        b"QUIT": {b"221", b"500"},
+        ## Transfer parameters
+        b"PORT": {b"200", b"500", b"501", b"421", b"530"},
+        b"PASV": {b"227", b"500", b"501", b"503" b"421", b"530"},
+        b"MODE": {b"200", b"500", b"501", b"503" b"421", b"530"},
+        b"TYPE": {b"200", b"500", b"501", b"503" b"421", b"530"},
+        b"STRU": {b"200", b"500", b"501", b"503" b"421", b"530"},
+        ## File Action Commands
+        b"ALLO": {b"200", b"202", b"500", b"501", b"504", b"421", b"530"},
+        b"REST": {b"500", b"501", b"502", b"421", b"530", b"350"},
+        b"STOR": {b"125", b"150", b"532", b"450", b"452", b"553", b"500", b"501", b"421", b"530"}, ## TODO: Has follow through response
+        b"STOU": {b"125", b"150", b"532", b"450", b"452", b"553", b"500", b"501", b"421", b"530"}, ## TODO: Has follow through response
+        b"RETR": {b"125", b"150", b"450", b"550", b"500", b"501", b"421", b"530"}, ## TODO: Has follow through response
+        b"LIST": {b"125", b"150", b"450", b"500", b"501", b"421", b"530"}, ## TODO: Has follow through response
+        b"NLIST": {b"125", b"150", b"450", b"500", b"501", b"421", b"530"}, ## TODO: Has follow through response
+        b"APPE": {b"125", b"150", b"532", b"450", b"452", b"550" b"553", b"500", b"501", b"502", b"421", b"530"}, ## TODO: Has follow through response
+        b"RNFR": {b"450", b"550",  b"500", b"501", b"502", b"421", b"530",  b"350"},
+        b"RNTO": {b"250",  b"532", b"553",  b"500", b"501", b"502", b"503", b"421", b"530"},
+        b"DELE": {b"250",  b"450", b"550",  b"500", b"501", b"502", b"421", b"530"},
+        b"RMD": {b"250",  b"500", b"501", b"502", b"421", b"530", b"550"},
+        b"MKD": {b"257",  b"500", b"501", b"502", b"421", b"530", b"550"},
+        b"PWD": {b"257",  b"500", b"501", b"502", b"421", b"550"},
+        b"ABOR": {b"225", b"226",  b"500", b"501", b"502", b"421"},
+        ## Informational commands
+        b"SYST": {b"215", b"500", b"501", b"502", b"421"},
+        b"STAT": {b"211", b"212", b"213", b"450", b"500", b"501", b"502", b"421", b"530"},
+        b"HELP": {b"211", b"214", b"500", b"501", b"502", b"421"},
+        ## Miscellaneous commands
+        b"SITE": {b"200", b"202", b"500", b"501", b"530"},
+        b"NOOP": {b"200", b"500", b"421"}
+
+    }
+
+    def __init__(self):
+        ## The last conn in this deque is the most recent PASV/PORT issued
+        ## Once a data connection has been completed or aborted, it is popleft() from this ds
+        self.dataConnSockets = []
+        self.dataConnTypes = []
+        self.dataConnCounter = 0
+        self.responseFollowThrough = collections.deque()
+
+    def simulateCommSequence(self, clientSock: socket.socket, commandSequence: Tuple[bytes], dataSequence: Tuple[bytes], dtsTimeout: Optional[Union[float, int]] = 0.2) -> None:
+        (flattenedCommandSequence, respondedCommandsCounter), (responseSequence, responseProccessedCounter) = self._simulateCommSequence(clientSock, commandSequence, dtsTimeout)
+        return (flattenedCommandSequence, respondedCommandsCounter), (responseSequence, responseProccessedCounter), (self.dataConnSockets, self.dataConnTypes)
+
+
+    def _extractDataTransfers(self) -> None:
+        raise NotImplementedError()
+        return None
+
+    
+    def _responseCallback(self, request: bytes, response: bytes) -> bool:
+        ## We are a ftp client here, we need to respond accordingly
+        req = re.search(b"^\w+", request).group(0)
+        resp = re.search(b"^\d+", response).group(0)
+        # print(f"responseCallback: req -> {str(request)}, resp_potential -> {str(response)}")
+        if req in self.dataConnControlCommands:
+            if req == b"PASV" and resp == b"227":
+                ## then we create a connection using resp detailss
+                pasvArgs = re.search(b"(\d+),(\d+),(\d+),(\d+),(\d+),(\d+)", response)
+                host = b".".join(pasvArgs.group(1,2,3,4))
+                port = int(pasvArgs.group(5)) * 256 + int(pasvArgs.group(6))
+
+                s = socket.socket()
+                s.connect((host, port))
+                self.dataConnSockets.append(s)
+                self.dataConnTypes.append("PASV")
+                return True
+
+            elif req == b"PORT" and resp == b"200":
+                ## A 200 means the server recognizes our PORT successfuly
+                ## - It does NOT mean that it will connect at this time however
+                ## - It can wait to connect until just before a data reliant command is issued
+                return True
+
+            elif req == b"ABORT" and resp in (b"225", b"226"):
+                ## TODO: What do we do if we recieve a failure on the ABORT command
+                raise NotImplementedError()
+                return True
+
+        
+
+        ## Check if it is an acceptable respone
+        if resp not in self.acceptableCommandResponses[req]:
+
+            ## If not, we check if it is an acceptable follow through
+            valid = False
+            acceptableFollowThroughs = self.acceptableCommandFollowThrough.get(req, {})
+            for acceptableResps in acceptableFollowThroughs.values():
+                if resp in acceptableResps:
+                    valid = True
+
+            ## if it's not a acceptable response or acceptable followthrough, we return false
+            if valid is False:
+                return False
+        else:
+             ## All informational messages 1x are followed through by a 2x or 4x, or 5x message
+            if resp.startswith(b"1"):
+                ## Therefore, we can skip these and check if the follow through message (or an original reply is observed)
+                return False
+
+
+        ## In case we haven't validated in the data connection initiation session, we do a standard valiation below
+        if req in self.dataConnReliantCommands:
+            ## Here, we assume we have a complete response that satisifies our connection
+            if resp.startswith(b"2") or resp == b"426":
+                if self.dataConnTypes[self.dataConnCounter] == "PORT":
+                    self.dataConnSockets[self.dataConnCounter] = self.dataConnSockets[self.dataConnCounter].accept()
+                self.dataConnCounter += 1
+
+            ## otherwise, it failed to be used, so data connection hasn't been consumed
+        else:
+            ## if this is a control only command, so we do nothing
+            pass
+
+        return True
+
+
+    def _requestCallback(self, request: bytes) -> None:
+        req = re.search(b"^\w+", request).group(0)
+
+        if req in self.dataConnControlCommands:
+            ## then we setup a connection
+            if req == b"PASV":
+                ## we sort this out when we receive a response (and the responseCallback is executed)
+                pass
+            elif req == b"PORT":
+                ## We retrieve the PORT args for host, port values
+                portArgs = re.search(b"^PORT (\d+),(\d+),(\d+),(\d+),(\d+),(\d+)\r\n", request)
+                host = b".".join(portArgs.group(1,2,3,4))
+                port = int(portArgs.group(5)) * 256 + int(portArgs.group(6))
+
+                ## we setup a new connection
+                s = socket.socket()
+                s.bind((host, port))
+                s.listen()
+
+                ## we then add it to the list
+                self.dataConnSockets.append(s)
+                self.dataConnTypes.append("PORT")
+
+            elif req == b"ABORT":
+                ## I need to read the ftp rfc to understand meticulously the behavior of ABORT
+                raise NotImplementedError()
+
+        elif req in self.dataConnReliantCommands:
+            ## At this point if we have preceeded this with a PORT command, we perform a accept()
+            # if req in self.dataSendCommands:
+            #     ## if it is a send command 
+            #     raise NotImplementedError()
+            #     pass
+            # elif req in self.dataRecvCommands:
+            #     ## then we do nothing for recv commands (on request callback)
+            #     pass
+            pass
+
+        return None
+
+
+def main() -> None:
+    cs = FTPClientSimulator()
+    clientSock = socket.socket()
+    clientSock.connect(("127.0.0.1", 21))
+    password = os.environ["SI_FTP_PASSWORD"]
+
+    ## Commands to issue to the server
+    commSequence = [
+        [b"USER SI_FTPTestUser1\r\n"],
+        [bytes(f"PASS {password}\r\n", "utf-8")],
+        [b"PASV\r\n"],
+        [b"LIST\r\n"],
+        [b"PORT 127,0,0,1,10,0\r\n"],
+        [b"LIST\r\n"]
+    ]
+
+    ## Data to send to the client
+    dataSequence = []
+
+    pprint.pprint(cs.simulateCommSequence(clientSock, commSequence, dataSequence))
+    return None
+
+if __name__ == "__main__":
+    main()
